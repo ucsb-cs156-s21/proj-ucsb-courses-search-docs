c0044247a4cc941bf08e71cda34303bf6c510353
pconrad <pconrad@users.noreply.github.com>
2022-02-02 01:39:56 +0000

    Deploying to main from @ ucsb-cs156-s21/proj-ucsb-courses-search@75884ccf95bb3f5c32220b3dbe45cbfd8d88bdee 🚀

---
+++ docs/storybook/precache-manifest.fa130d522cb7c9db0cc2e76e90475e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a91e97c10b56b68345a393c8c04dec4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.52ab713f970f16792a01.bundle.js"
+  },
+  {
+    "url": "runtime~main.52ab713f970f16792a01.bundle.js"
+  },
+  {
+    "revision": "a890304a4f80ab1c8b9e",
+    "url": "static/css/main.d9a6bce8.chunk.css"
+  },
+  {
+    "revision": "9a9c294d7c72d4232b2d",
+    "url": "static/css/vendors~main.6414bb7b.chunk.css"
+  },
+  {
+    "url": "vendors~main.52ab713f970f16792a01.bundle.js"
+  },
+  {
+    "url": "vendors~main.52ab713f970f16792a01.bundle.js.LICENSE.txt"
+  }
+]);
